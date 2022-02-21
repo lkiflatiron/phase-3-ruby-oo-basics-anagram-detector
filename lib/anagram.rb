3e0246dfdb282f74e2238168e80e92869acf0dda
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 19:08:43 -0500

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def sort_chars word
+    word.chars.sort{ |a, b| a.upcase <=> b.upcase }.join('')
+  end
+
+  def match(word_array)
+    #word_array.any? { | entry| sort_chars(@word) == sort_chars(entry) }
+    word_array.filter { | entry| sort_chars(@word) == sort_chars(entry)}
+  end
+end
+
+listen = Anagram.new("listen")
+p listen.match(%w[enlists google inlets banana])
